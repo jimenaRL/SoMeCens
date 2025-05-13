2f6d64637961c1f1739d9bdcf929d05304d3ab15
jimenaRL <jimena@melix.net>
2025-05-13 17:44:42 +0200

    Modify files hehe

--- somecens/nuts/conf.py
+++ somecens/nuts/conf.py
@@ -1,32 +1,37 @@
 import os
 import yaml
 
-DIRPATH = os.path.dirname(os.path.realpath(__file__))
-DATAFOLDERPATH =  os.path.join(DIRPATH, 'data')
+DIR = os.path.dirname(os.path.realpath(__file__))
+DATAFOLDER =  os.path.join(DIR, 'data')
 
-NUTSLEVELS = [1, 2, 3]
+LEVELS = [1, 2, 3]
 
-NUTSFILES = {
-    'nuts1': 'NUTS2021-NUTS2024_codes_NUTS123_to_NUTS1.xlsx - NUTS2024.csv',
-    'statistical_regions': 'NUTS2021-NUTS2024_codes_NUTS123_to_NUTS1.xlsx - Statistical Regions.csv',
+FILES = {
+    # 'nuts1': 'NUTS2021-NUTS2024_codes_NUTS123_to_NUTS1.xlsx - NUTS2024.csv',
+    # 'statistical_regions': 'NUTS2021-NUTS2024_codes_NUTS123_to_NUTS1.xlsx - Statistical Regions.csv',
     'allLevels': 'NUTS2021-NUTS2024_codes_NUTS123_to_NUTS1.xlsx - NUTS2021- NUTS2024.csv',
-    'nutsCyrillicGreekLatin': 'NUTS2021-NUTS2024_codes_NUTS123_to_NUTS1.xlsx - Cyrillic & Greek to Latin.csv',
+    # 'nutsCyrillicGreekLatin': 'NUTS2021-NUTS2024_codes_NUTS123_to_NUTS1.xlsx - Cyrillic & Greek to Latin.csv',
     'nuts3_total': 'NUTS123_Y2015_2024_Gender.xlsx - Sheet 1_Total.csv',
     'nuts3_males': 'NUTS123_Y2015_2024_Gender.xlsx - Sheet 2_Males.csv',
     'nuts3_females': 'NUTS123_Y2015_2024_Gender.xlsx - Sheet 3_Females.csv',
     'codes_country': 'nuts_codes_country.yml',
-    'flattenStructure': 'nuts_flatten.csv',
-    'flattenAgeDistributions': 'nuts_age_latten.csv',
+    'flattenStructure2021': 'nuts_flatten_2021.csv',
+    'flattenStructure2024': 'nuts_flatten_2024.csv',
+    'flattenGenderDistributions2024': 'nuts_gender_flatten_2024.csv',
+    'flattenGenderDistributions2021': 'nuts_gender_flatten_2021.csv',
 }
 
-NUTSPATH = os.path.join(DATAFOLDERPATH, NUTSFILES['allLevels'])
-NUTSCODESPATH = os.path.join(DATAFOLDERPATH, NUTSFILES['codes_country'])
-NUTSFLATTENPATH = os.path.join(DATAFOLDERPATH, NUTSFILES['flattenStructure'])
-NUTS3TOTAL = os.path.join(DATAFOLDERPATH, NUTSFILES['nuts3_total'])
-NUTS3MALES = os.path.join(DATAFOLDERPATH, NUTSFILES['nuts3_males'])
-NUTS3FEMALES = os.path.join(DATAFOLDERPATH, NUTSFILES['nuts3_females'])
-NUTS3AGEFLATTENPATH = os.path.join(DATAFOLDERPATH, NUTSFILES['flattenAgeDistributions'])
+ALLLEVELS = os.path.join(DATAFOLDER, FILES['allLevels'])
+CODES = os.path.join(DATAFOLDER, FILES['codes_country'])
+FLATTEN2021 = os.path.join(DATAFOLDER, FILES['flattenStructure2021'])
+FLATTEN2024 = os.path.join(DATAFOLDER, FILES['flattenStructure2024'])
 
-with open(NUTSCODESPATH, "r") as fh:
+NUTS3GENDERMALES = os.path.join(DATAFOLDER, FILES['nuts3_males'])
+NUTS3GENDERFEMALES = os.path.join(DATAFOLDER, FILES['nuts3_females'])
+NUTS3GENDERTOTAL = os.path.join(DATAFOLDER, FILES['nuts3_total'])
+NUTS3GENDERFLATTEN2024 = os.path.join(DATAFOLDER, FILES['flattenGenderDistributions2024'])
+NUTS3GENDERFLATTEN2021 = os.path.join(DATAFOLDER, FILES['flattenGenderDistributions2021'])
+
+with open(CODES, "r") as fh:
     CODESCOUNTRY = yaml.load(fh, Loader=yaml.SafeLoader)
 COUNTRYCODES = {v: k for k,v in CODESCOUNTRY.items()}
--- somecens/nuts/tools.py
+++ somecens/nuts/tools.py
@@ -2,27 +2,37 @@ import csv
 from subprocess import Popen, PIPE
 
 from somecens.nuts.conf  import \
-    NUTSFLATTENPATH, \
+    NUTS3GENDERFLATTEN2021, \
+    NUTS3GENDERFLATTEN2024, \
+    NUTS3GENDERTOTAL, \
+    NUTS3GENDERFEMALES, \
+    NUTS3GENDERMALES, \
+    FLATTEN2021, \
+    FLATTEN2024, \
     COUNTRYCODES, \
-    NUTSLEVELS, \
-    NUTSPATH
+    LEVELS, \
+    ALLLEVELS
 
-def strToInt(str: str) -> int:
-    return int(srt.replace('\u202f', ''))
+def strToInt(string: str) -> int:
+    return int(string.replace('\u202f', ''))
 
-def getNutsLocationsLevel(country, level) -> str:
+def getNutsLocationsLevel(country: str, level: int, year: int = 2024) -> str:
     code = COUNTRYCODES[country]
     f1 = f"col('Country Code') eq '{code}'"
     f2 = f"col('NUTS level') eq {level}"
+    nf3 = f"col('Code {year}') eq ''"
     s = f"NUTS level {level}"
-    p1 = Popen(["xan", "filter", f1, NUTSPATH], stdout=PIPE)
+    p1 = Popen(["xan", "filter", f1, ALLLEVELS], stdout=PIPE)
     p2 = Popen(["xan", "filter", f2], stdin=p1.stdout, stdout=PIPE)
-    p3 = Popen(["xan", "select", s], stdin=p2.stdout, stdout=PIPE)
-    output = p3.communicate()[0].decode().split('\n')[1:-1]
+    p3 = Popen(["xan", "filter", "--invert-match", nf3], stdin=p2.stdout, stdout=PIPE)
+    p4 = Popen(["xan", "select", s], stdin=p3.stdout, stdout=PIPE)
+    output = p4.communicate()[0].decode().split('\n')[1:-1]
     return output
 
-def getNutsLocations(country, format='dict'):
-    locsDict = {level: getNutsLocationsLevel(country, level) for level in NUTSLEVELS}
+def getNutsLocations(country: str, format: str = 'dict', year: int = 2024):
+    locsDict = {
+        level: getNutsLocationsLevel(country, level, year)
+        for level in LEVELS}
     if format == 'dict':
         return locsDict
     elif format == 'flatten':
@@ -30,7 +40,72 @@ def getNutsLocations(country, format='dict'):
     else:
         raise ValueError(f"Format must be 'dict' or 'flatten'. Found {format}")
 
-def getNuts(country):
-    code = COUNTRYCODES[country]
-    with open(NUTSFLATTENPATH, newline='') as csvfile:
-        return [d for d in csv.DictReader(csvfile) if d['country_code'] == code]
+def getUnits(country: str | None = None, year: int = 2024):
+    path = globals()[f"FLATTEN{year}"]
+    with open(path, newline='') as csvfile:
+        units = [d for d in csv.DictReader(csvfile)]
+    if country:
+        code = COUNTRYCODES[country]
+        return [d for d in units if d['country_code'] == code]
+    else:
+        return units
+
+def getCountryCode(code: str) -> str:
+    return code[:2]
+
+def getParentCode(code: str) -> str:
+    return code[:-1]
+
+def flattenNutsGenderDistributions(year: int = 2024) -> None:
+    with open(NUTS3GENDERTOTAL) as tf:
+        tD = [d for d in csv.DictReader(tf)]
+    with open(NUTS3GENDERMALES) as mf:
+        mD = [d for d in csv.DictReader(mf)]
+    with open(NUTS3GENDERFEMALES) as ff:
+        fD = [d for d in csv.DictReader(ff)]
+    codes = [u['code'] for u in getUnits(year=year)]
+    gDist = []
+    for code in codes:
+        gd = {'code': code, 'year': year}
+        for m in mD:
+            if m['code'] == code:
+                try:
+                    gd['male'] = strToInt(m[str(year)])
+                except:
+                    continue
+        for f in fD:
+            if f['code'] == code:
+                try:
+                    gd['female'] = strToInt(f[str(year)])
+                except:
+                    continue
+        for t in tD:
+            if t['code'] == code:
+                try:
+                    gd['total'] = strToInt(t[str(year)])
+                except:
+                    continue
+        if len(gd) == 5:
+            gDist.append(gd)
+        else:
+            print(f"No suitable gendre data found, skipping code {code}")
+
+    outpath = globals()[f"NUTS3GENDERFLATTEN{year}"]
+    with open(outpath, 'w', newline='') as csvfile:
+        headers = ['total', 'code', 'female', 'year', 'male']
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+        for row in gDist:
+            writer.writerow(row)
+    print(f"Flatten file saved at {outpath}")
+
+def getNutsGenderDistributions(country: str | None = None, year: int = 2024):
+    path = globals()[f"NUTS3GENDERFLATTEN{year}"]
+    with open(path, 'r') as csvfile:
+        genderDistDicts = [d for d in csv.DictReader(csvfile)]
+    if country:
+        country_code = COUNTRYCODES[country]
+        return [d for d in genderDistDicts if d['code'][:2] == country_code]
+    else:
+        return genderDistDicts
+
