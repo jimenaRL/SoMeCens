4bf7797e7ac9c8b22863c1b62873bd579ae1738b
jimenaRL <jimena@melix.net>
2025-05-13 18:22:40 +0200

    Add method to import gender distriibution

--- somecens/demograph.py
+++ somecens/demograph.py
@@ -11,7 +11,7 @@ DEFAULTAGECATS = {
 DEFAULTGENDERCATS = {
     'male',
     'female',
-    'total'
+    'total',
 }
 
 class GeoUnit:
@@ -24,15 +24,17 @@ class GeoUnit:
         label: str ,
         level: int,
         code: str,
-        age_categories: list[str] = DEFAULTGENDERCATS,
-        gender_categories: list[str] = DEFAULTGENDERCATS,
+        age_categories: set[str] = DEFAULTGENDERCATS,
+        gender_categories: set[str] = DEFAULTGENDERCATS,
         ) -> None:
         self.label = label
         self.code = code
         self.level = level
         self.children = []
-        self.ageDistribution = age_categories
-        self.genderDistribution = gender_categories
+        self.ageCategories = age_categories
+        self.ageDistribution = None
+        self.genderCategories = gender_categories
+        self.genderDistribution = None
 
     def __str__(self) -> str:
         s = f"GeoUnit {self.label}\n\tlevel: {self.level}\n\tcode: {self.code}"
@@ -45,6 +47,8 @@ class GeoUnit:
         s += f"\n{indent}GeoUnit {self.label}"
         s += f"\n{indent}level: {self.level}"
         s += f"\n{indent}code: {self.code}"
+        s += f"\n{indent}gender distribution: {self.genderDistribution}"
+        s += f"\n{indent}age distribution: {self.ageDistribution}"
         codes = ' | '.join([child.code for child in self.children])
         if self.children:
             s += f"\n{indent}children: {codes}"
@@ -57,19 +61,42 @@ class GeoUnit:
         return self.children
 
     def setAgeDistribution(ageDistribution: dict) -> None:
-        assert ageDistribution.keys == self.ageCategories
+        assert self.ageCategories.issubset(set(ageDistribution.keys))
         assert abs(sum(ageDistribution.values) - 100) < self.ageDistTol
         self.ageDistribution = ageDistribution
 
-    def setGenderDistribution(genderDistribution: dict) -> None:
-        assert genderDistribution.keys == self.genderCategories
-        assert abs(sum(genderDistribution.values) - 100) < self.genderDistTol
-        self.genderDistribution = genderDistribution
+    def setGenderDistribution(self, genderDistribution: dict) -> None:
+        assert self.genderCategories.issubset(set(genderDistribution.keys()))
+        gd = {k: genderDistribution[k] for k in self.genderCategories}
+        self.genderDistribution = gd
 
 class DemoGraph:
 
     demoKeys = {'country_code', 'label', 'level', 'code', 'parent_code'}
-    defaultGenderCategories = DEFAULTGENDERCATS
+    genderCategories = DEFAULTGENDERCATS
+
+    def __init__(
+            self,
+            demography: Iterable[Dict],
+            genderCats: Iterable[str] | None = DEFAULTGENDERCATS) -> None:
+        """
+        demography: iterable of dicts
+        Keys and values must be strings and dict keys equal to self.demoKeys
+        genderCats: iterable of strings
+        """
+        self.checkDemography(demography)
+        country, code = self._getCountryAndCode(demography)
+        self.country = country
+        self.countryCode = code
+        self.demography = demography
+        self.rootGeoUnit = None
+        self.buildGeoTree()
+
+        self.genderCategories = genderCats
+        # self.ageDistribution = age_categories
+
+    def __str__(self) -> str:
+        return f"{self.country.capitalize()} ({self.countryCode}) DemoGraph"
 
     def _showGeoUnits(self, indent: int = 0, geoUnit: GeoUnit | None = None) -> None:
         if geoUnit:
@@ -81,7 +108,7 @@ class DemoGraph:
     def showGeoUnits(self) -> None:
         self._showGeoUnits(0, self.rootGeoUnit)
 
-    def getCountryAndCode(self, demography):
+    def _getCountryAndCode(self, demography) -> (str, str):
         for d in demography:
             if d['level'] == '0':
                 return d['label'], d['code']
@@ -100,7 +127,7 @@ class DemoGraph:
             mssg = f"Found {nb_first_levels} firt levels. Must be only one."
             raise ValueError(mssg)
 
-    def _getGeoUnit(self, geoUnit, code: str):
+    def _getGeoUnit(self, geoUnit, code: str) -> Type[GeoUnit]:
         if geoUnit.code == code:
             return geoUnit
         for child in geoUnit.children:
@@ -108,48 +135,41 @@ class DemoGraph:
             if geoUnit:
                 return geoUnit
 
-    def getGeoUnit(self, code: str):
+    def getGeoUnit(self, code: str) -> Type[GeoUnit]:
         return self._getGeoUnit(geoUnit=self.rootGeoUnit, code=code)
 
     def checkGenderDistributions(self, genderDistribution: Iterable[Dict]) -> None:
+        # check dicts keys
+        dk = set([set(d.keys()) ==  self.genderCategories for d in genderDistribution])
         # check dicts's keys instances
-        kt = set([isinstance(k, str) for d in demography for k in d.keys()])
+        kt = set([isinstance(k, str) for d in genderDistribution for k in d.keys()])
+        assert kt == {True}
         # check that dicts's values are convertible to float
         try:
-            gD = [{k: float(v)} for d in demography for k, v in d.items()]
+            [float(d[k]) for d in genderDistribution for k in ['male', 'female', 'total']]
         except Exception as e:
             raise ValueError(
                 """Unnable to parse genderDistribution.
                 Please check that all values can be converted to float.""")
-        # check dicts keys
-        cats = self.genderCategories.union('code')
-        dk = set([set(d.keys()) ==  cats for d in dD])
-        assert dk == {True}
         # check that all geoUnits are present in the gender distribution
-        # TO DO
+        codes = {gd['code'] for gd in genderDistribution}
+        assert codes == {gd['code'] for gd in self.demography}
 
-    def __init__(
-            self,
-            demography: Iterable[Dict],
-            genderCats: Iterable[str] | None = defaultGenderCategories) -> None:
-        """
-        demography: iterable of dicts
-        Keys and values must be strings and dict keys equal to self.demoKeys
-        genderCats: iterable of strings
-        """
-        self.checkDemography(demography)
-        country, code = self.getCountryAndCode(demography)
-        self.country = country
-        self.countryCode = code
-        self.demography = demography
-        self.rootGeoUnit = None
-        self.buildGeoTree()
+    def setGenderDistributions(self, genderDistribution: Iterable[Dict]) -> None:
+        self.checkGenderDistributions(genderDistribution)
+        self._setGenderDistributions(self.rootGeoUnit, genderDistribution)
+        return
 
-        self.genderCategories = genderCats
-        # self.ageDistribution = age_categories
 
-    def __str__(self) -> str:
-        return f"{self.country.capitalize()} ({self.countryCode}) DemoGraph"
+    def _setGenderDistributions(self, geoUnit: Type[GeoUnit], genderDistribution: Iterable[Dict]) -> None:
+        # we already check that all codes are presents in the distribution
+        ugd = [gd for gd in genderDistribution if gd['code'] == geoUnit.code][0]
+        geoUnit.setGenderDistribution(ugd)
+        for child in geoUnit.children:
+            geoUnit = self._setGenderDistributions(child, genderDistribution)
+            # if geoUnit:
+            #     return geoUnit
+
 
     def buildGeoTree(self) -> None:
         max_level = max(map(int, set([d['level'] for d in self.demography])))
