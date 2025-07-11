c3ebac0eba138dd351c3091f12fcf565968585a1
jimenaRL <jimena@melix.net>
2025-07-11 16:26:20 +0200

    Add test data fomder

--- tests/test_demograph.py
+++ tests/test_demograph.py
@@ -1,1 +1,31 @@
+# =============================================================================
+# Demograph tests
+# =============================================================================
 #
+
+import os
+import json
+import yaml
+
+from somecens import DemoGraph
+
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# geounits_path = os.path.join(dir_path, "data", "geounits.yml")
+DATADIR = os.path.join("/home/jimena/work/dev/SoMeCens/tests/data")
+
+with open(os.path.join(DATADIR, "geounits.yml")) as f:
+    GEOUNITS = yaml.safe_load(f)
+
+for g in GEOUNITS:
+    print(json.dumps(g, indent=2))
+
+with open(os.path.join(DATADIR, "subunits.yml")) as f:
+    SUBUNITS = yaml.safe_load(f)
+
+print(json.dumps(SUBUNITS, indent=2))
+
+def test_demograph():
+    demo = DemoGraph(demography=GEOUNITS)
+    demo.setSubUnitsNames(SUBUNITS)
+
+
--- tests/test_nuts.py
+++ tests/test_nuts.py
@@ -1,3 +1,8 @@
+# =============================================================================
+# NUTS tools tests
+# =============================================================================
+#
+
 import json
 import random
 
@@ -8,35 +13,44 @@ from somecens.nuts.tools import \
     getNutsAgeDistributions, \
     getNutsGenderDistributions
 
-def test_geounits(country, year):
+COUNTRY = "france"
+YEAR = 2024
+
+def test_geounits(country=COUNTRY, year=YEAR):
     dicts = getUnits(country=country, year=year)
     l = len(dicts)
     n = random.randint(0, l)
     unit_dict = dicts[n]
+    assert type(dicts) == list
+    assert type(unit_dict) == dict
     print("--- getUnits test")
     print(f"There are {l} dicts entries of the form:")
     print(json.dumps(unit_dict, indent=2))
 
-def test_laus(country):
+def test_laus(country=COUNTRY):
     laus = getLaus(country=country)
     l = len(laus)
     n = random.randint(0, l)
     key = list(laus.keys())[n]
     value = list(laus.values())[n]
+    assert type(laus) == dict
+    assert type(value) == list
     print("--- getLaus test")
     print(f"Dictionary of LAUS contains {l} entries of the form:")
     # print(f"\t{key}: {value}")
     print(json.dumps({key: value}, indent=2))
 
-def test_age_distribution(country, year):
+def test_age_distribution(country=COUNTRY, year=YEAR):
     aDist = getNutsAgeDistributions(country=country, year=year)
     l = len(aDist)
     n = random.randint(0, l)
+    assert type(aDist) == list
+    assert type(aDist[n]) == dict
     print("--- getNutsAgeDistributions test")
     print(f"Age distributions list contains {len(aDist)} entries of the form:")
     print(json.dumps(aDist[n], indent=2))
 
-def test_gender_distribution(country, year):
+def test_gender_distribution(country=COUNTRY, year=YEAR):
     gDist = getNutsGenderDistributions(country=country, year=year)
     l = len(gDist)
     n = random.randint(0, l)
@@ -46,8 +60,6 @@ def test_gender_distribution(country, year):
 
 if __name__ == "__main__":
 
-    year = 2024
-    country = 'france'
     s = f"----- NUTS MODULE TEST | COUNTRY {country} | NUTS {year} -----"
     print(f"{'-' * len(s)}\n{s}\n{'-' * len(s)}")
 
