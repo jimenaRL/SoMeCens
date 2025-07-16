75ac108da483b56fd0b13fde894f87209343c7f9
jimenaRL <jimena@melix.net>
2025-07-16 10:32:47 +0200

    Add epo tools test

--- test/test_epo.py
+++ test/test_epo.py
@@ -1,1 +1,27 @@
-# Need to create a sqlite dataset with test data
+# =============================================================================
+# EPO databases tools tests
+# =============================================================================
+#
+
+import os
+import pytest
+
+from somecens.epo.tools import getMetadata
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+DBPATH = os.path.join(dir_path, "data", "test_france_2023.db")
+COLUMNS = ['pseudo_id', 'location', 'screen_name']
+
+def test_getMetadata(dbpath = DBPATH, columns = COLUMNS):
+
+    metadata = getMetadata(dbpath, columns)
+    assert type(metadata) == list
+    assert len(metadata[0]) == len(COLUMNS)
+
+    badpath = "qehfzefasashvze.db"
+    with pytest.raises(Exception) as exc_info:
+        getMetadata(badpath, columns)
+    assert exc_info.value.args[0] == f"Unnable to find database at {badpath}"
+
+if __name__ == "__main__":
+    test_getMetadata()
