668dccef6b920df38072fa5da99294df48f18932
jimenaRL <jimena@melix.net>
2025-07-17 11:37:51 +0200

    Update test

--- test/test_epo.py
+++ test/test_epo.py
@@ -11,12 +11,20 @@ from somecens.epo.tools import getMetadata
 dir_path = os.path.dirname(os.path.realpath(__file__))
 DBPATH = os.path.join(dir_path, "data", "test_belgium_2020.db")
 COLUMNS = ['pseudo_id', 'location', 'screen_name']
+LIMIT = 11
+NOTNULL = "location"
 
-def test_getMetadata(dbpath = DBPATH, columns = COLUMNS):
+def test_getMetadata(dbpath = DBPATH, columns = COLUMNS, limit = LIMIT):
 
-    metadata = getMetadata(dbpath, columns)
+    metadata = getMetadata(
+        dbpath,
+        columns=columns,
+        not_null=NOTNULL,
+        limit=limit)
+
     assert type(metadata) == list
     assert len(metadata[0]) == len(COLUMNS)
+    assert len(metadata) == limit
 
     badpath = "qehfzefasashvze.db"
     with pytest.raises(Exception) as exc_info:
