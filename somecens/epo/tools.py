188f88de271aa4d4e0dc3fc4988c34ec45eb68f7
jimenaRL <jimena@melix.net>
2025-07-17 11:34:17 +0200

    Add NOT NULL and LIMIT to sqlite metadata query

--- somecens/epo/tools.py
+++ somecens/epo/tools.py
@@ -7,7 +7,7 @@ from string import Template
 
 from somecens.epo.conf import METADATAFIELDS, METADATATABLE
 
-def getLastRelease(pathPattern: str, db: str, country: str, year: int):
+def getLastRelease(pathPattern: str, db: str, country: str, year: int) -> str:
     path = Template(pathPattern).substitute(db=db, country=country, year=year)
     canditates_paths = glob(path)
     if len(canditates_paths) == 0:
@@ -17,16 +17,28 @@ def getLastRelease(pathPattern: str, db: str, country: str, year: int):
     print(f"Found last release at {last_release_path}.")
     return last_release_path
 
-def getMetadata(dbpath: str, columns:  Iterable[str] | None = None):
+def getMetadata(
+        dbpath: str,
+        limit: int | None = None,
+        not_null: str | None = None,
+        columns:  Iterable[str] | None = None) -> list(tuple):
     if not os.path.exists(dbpath):
         raise ValueError(f"Unnable to find database at {dbpath}")
     if not columns:
         columns = METADATAFIELDS
     joinedColumns = ','.join(columns)
-    query = f"SELECT {joinedColumns} FROM {METADATATABLE}"
+    query = f"SELECT {joinedColumns} FROM {METADATATABLE} "
+    if not_null:
+        query += f"WHERE {not_null} IS NOT NULL "
+    if limit:
+        query += f"LIMIT {limit} "
     with sqlite3.connect(dbpath) as con:
         cur = con.cursor()
         cur.execute(query)
         res = cur.fetchall()
-    print(f"Found {len(res)} rows in metadata with columns {joinedColumns}.")
+
+    msg = f"Found {len(res)} rows in metadata with columns {joinedColumns}"
+    if not_null:
+        msg += f" and not NULL column {not_null}"
+    print(msg)
     return res
