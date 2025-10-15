cfe63e8ee50926bfff317b48f95aaff35a14a8e7
jimenaRL <jimena@melix.net>
2025-10-15 13:19:25 +0200

    Updata test and test data

--- test/test_matchs.py
+++ test/test_matchs.py
@@ -5,26 +5,31 @@ import json
 
 from somecens import DemoGraph
 from somecens.tools import \
+    reverseSearch, \
+    searchOccurrence, \
     searchOccurrences, \
     matchUsersLocations
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-DATADIR = os.path.join(dir_path, "data")
+TESTDATADIR = os.path.join(dir_path, "data")
 
-with open(os.path.join(DATADIR, "geounits.yml")) as f:
+with open(os.path.join(TESTDATADIR, "geounits.yml")) as f:
     GEOUNITS = yaml.safe_load(f)
 
-with open(os.path.join(DATADIR, "subunits.yml")) as f:
+with open(os.path.join(TESTDATADIR, "subunits.yml")) as f:
     SUBUNITS = yaml.safe_load(f)
 
-with open(os.path.join(DATADIR, "metadata.csv")) as f:
+with open(os.path.join(TESTDATADIR, "metadata.csv")) as f:
     METADATA = [r for r in csv.reader(f)]
 
-with open(os.path.join(DATADIR, "user_locations.yml")) as f:
+with open(os.path.join(TESTDATADIR, "user_locations.yml")) as f:
     USERLOCATIONS = yaml.safe_load(f)
 
-with open(os.path.join(DATADIR, "expected_match_numbers.yml")) as f:
+with open(os.path.join(TESTDATADIR, "expected_match_numbers.yml")) as f:
     MATCHNUMBERS = yaml.safe_load(f)
+
+BANNEDREGEXFILE = os.path.join(TESTDATADIR, "country_regex.txt")
+CSVFILE = os.path.join(TESTDATADIR, "metadata.csv")
 
 with open('somecens/data/flags_unicode_emoji.json', 'r') as f:
     flags_unicode_emojis = json.load(f)
@@ -37,11 +42,13 @@ for f in french_flag_emojis:
     banned_emojis.remove(f)
 
 BANNEDEMOJIES = list(flags_unicode_emojis.values())
+BANNEDWORDS = ["Le Caire", "Australia"]
+
 
 DEMO = DemoGraph(demography=GEOUNITS)
 DEMO.setSubUnitsNames(SUBUNITS)
 LOCATIONSDICT = {d["code"]: d["label"] for d in GEOUNITS}
-LOCATIONSDICTGROUPS = DEMO.getAllSubUnits()
+LOCATIONSDICTGROUPS = DEMO.getAllSubUnits(max_level=3)
 
 MATCHKWARGS = {
     "stopwords": ["le", "la", "de"],
@@ -50,66 +57,79 @@ MATCHKWARGS = {
     "has_headers": True
 }
 
-BANNEDWORDS = ["États-Unis", "Egypte"]
+SEARCHCOL = "location"
 
+def test_reverseSearch():
+    output = reverseSearch(
+        csvfile=CSVFILE,
+        search_col=SEARCHCOL,
+        banned_regex_file=BANNEDREGEXFILE)
+    # when countin lined of parsed output remove 1 for trailing '\n' and
+    # another 1 for headers
+    nb_not_removed = len(output.split("\n")) -1 - 1
+    assert nb_not_removed == 43
 
 def test_searchOccurrences():
 
-    file_path = os.path.join(DATADIR, "metadata.csv")
     search_col = "location"
 
-    regex = "\\bLe Caire\\b"
-    matchs = searchOccurrences(
-        file_path,
-        regex=regex,
-        search_col=search_col,
-        banned_regex=["\\bEgypte\\b"])
-    assert len(matchs) == 0
-
     regex = ["\\bVal-d'Oise\\b", "\\bArica\\b"]
     matchs = searchOccurrences(
-        file_path,
+        csvfile=CSVFILE,
         regex=regex,
-        search_col=search_col)
+        search_col=SEARCHCOL,
+        allowed_emojis=[],
+        banned_emojis=[],
+    )
     assert len(matchs) == 1
 
-
     regex = ["\\bVal-d'Oise\\b", "\\bArica\\b"]
     matchs = searchOccurrences(
-        file_path,
+        csvfile=CSVFILE,
         regex=regex,
-        search_col=search_col)
+        search_col=SEARCHCOL,
+        allowed_emojis=[],
+        banned_emojis=[],
+    )
     assert len(matchs) == 1
 
     BANNEDEMOJIES.remove(':France:')
 
     regex = ["\\bArica\\b", "\\bFrance\\b",]
     matchs = searchOccurrences(
-        file_path,
+        csvfile=CSVFILE,
         regex=regex,
-        search_col=search_col)
+        search_col=SEARCHCOL,
+        allowed_emojis=[],
+        banned_emojis=[],
+    )
     assert len(matchs) == 7
     matchs = searchOccurrences(
-        file_path,
+        csvfile=CSVFILE,
         regex=regex,
-        banned_emojis=BANNEDEMOJIES,
+        search_col=SEARCHCOL,
         allowed_emojis=french_flag_emojis,
-        search_col=search_col)
+        banned_emojis=BANNEDEMOJIES,
+    )
     assert len(matchs) == 6
 
     regex = ["\\bBordeaux\\b",]
     matchs = searchOccurrences(
-        file_path,
+        csvfile=CSVFILE,
         regex=regex,
-        search_col=search_col)
+        search_col=SEARCHCOL,
+        allowed_emojis=[],
+        banned_emojis=[],
+    )
     assert len(matchs) == 1
     matchs = searchOccurrences(
-        file_path,
+        csvfile=CSVFILE,
         regex=regex,
+        search_col=SEARCHCOL,
+        allowed_emojis=[],
         banned_emojis=BANNEDEMOJIES,
-        search_col=search_col)
+    )
     assert len(matchs) == 0
-
 
 
 def test_matchUsersLocations():
@@ -119,20 +139,22 @@ def test_matchUsersLocations():
         locations=LOCATIONSDICT,
         data=METADATA,
         banned_words=BANNEDWORDS,
+        allowed_emojis=french_flag_emojis,
+        banned_emojis=BANNEDEMOJIES,
         **MATCHKWARGS
     )
 
     for code, label in LOCATIONSDICT.items():
-        s = f"""
-            ------------
-            code: {code}
-            label: {label}
-            # users expected: {MATCHNUMBERS[code]}
-            # matched users: {len(matchedUsersLocs[code])}
-            matchs:
-            \t{'\n\t\t'.join([u[1] + " | " + u[2] for u in matchedUsersLocs[code]])}
-            ------------"""
-        print(s)
+        # s = f"""
+        #     ------------
+        #     code: {code}
+        #     label: {label}
+        #     # users expected: {MATCHNUMBERS[code]}
+        #     # matched users: {len(matchedUsersLocs[code])}
+        #     matchs:
+        #     \t{'\n\t\t'.join([u[1] + " | " + u[2] for u in matchedUsersLocs[code]])}
+        #     ------------"""
+        # print(s)
         if MATCHNUMBERS[code] != len(matchedUsersLocs[code]):
             m = f"Expected {MATCHNUMBERS[code]} matchs in data for location "
             m += f"'{code}: {label}', found {len(matchedUsersLocs[code])}:"
@@ -149,21 +171,21 @@ def test_matchMultipleUsersLocations():
         banned_words=BANNEDWORDS,
         **MATCHKWARGS
     )
-
-    for code, labels_list in LOCATIONSDICTGROUPS.items():
-        s = f"""
-            ------------
-            code: {code}
-            labels list: {labels_list}
-            XXXX: {LOCATIONSDICT[code]}
-            # matched users: {len(multiUsersLocs[code])}
-            matchs:
-                \t{'\n\t\t'.join([u[1] + " | " + u[2] for u in multiUsersLocs[code]])}
-            ------------"""
-        print(s)
+    # for code, labels_list in LOCATIONSDICTGROUPS.items():
+    #     s = f"""
+    #         ------------
+    #         code: {code}
+    #         labels list: {labels_list}
+    #         XXXX: {LOCATIONSDICT[code]}
+    #         # matched users: {len(multiUsersLocs[code])}
+    #         matchs:
+    #             \t{'\n\t\t'.join([u[1] + " | " + u[2] for u in multiUsersLocs[code]])}
+    #         ------------"""
+    #     print(s)
 
 
 if __name__ == "__main__":
+    test_reverseSearch()
     # test_searchOccurrences()
-    test_matchUsersLocations()
+    # test_matchUsersLocations()
     # test_matchMultipleUsersLocations()
