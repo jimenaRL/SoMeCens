26b4bcc482a748e156c6b98c1a21b39f83cf4079
Jimena Royo-Letelier <jimena@melix.net>
2025-12-04 11:11:58 +0000

    Fixmissing paths

--- scripts/consolidate_nuts_results.py
+++ scripts/consolidate_nuts_results.py
@@ -30,11 +30,14 @@ for level in [0, 1, 2, 3]:
             continue
 
         folder = os.path.join(resultsfolder, country)
-        if not os .path.exists(folder):
-            # print(f"Skipping country {country}. Dind't find folder {folder}")
+        if not os.path.exists(folder):
+            print(f"Skipping country {country}. Dind't find folder {folder}")
             continue
 
         percpath = os.path.join(folder, f"nb_matchs_perc_{country}_nuts_2024_epo_{year}_level_{level}.csv")
+        if not os.path.exists(percpath):
+            print(f"Skipping country {country}. Dind't find file {percpath}")
+            continue
         with open(percpath) as f:
             percdata.extend([l.replace('\n', '') for l in f.readlines()])
 
